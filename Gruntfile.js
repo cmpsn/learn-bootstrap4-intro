1fa1d07fbc0b1f5894ed3cee76c3f6e936d7dcaf
Camil Pos <camilpos19@gmail.com>
2020-09-19 14:46:34 +0300

    Grunt - part 1

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = function(grunt) {
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+  
+  // Automatically load required Grunt tasks
+  require('jit-grunt')(grunt);
+
+  // declare sass
+  const sass = require("node-sass");
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+    sass: {
+      options: {
+        implementation: sass,
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/styles.css': 'css/styles.scss'
+        }
+      }
+    },
+    watch: {
+      files: 'css/*.scss',
+      tasks: ['sass']
+    },
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src: [
+            'css/*.css',
+            '*.html',
+            'js/*.js'
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: './'
+          }
+        }
+      }
+    }
+  });
+
+  // Register the SASS task (at line 12)
+  grunt.registerTask('css', ['sass']);
+  
+  // Register the watch and browserSync tasks
+  // The WATCH task MUST be the LAST task always,
+  // otherwise watch will stop all other tasks that comes after it
+  grunt.registerTask('default', ['browserSync', 'watch']);
+};
